d9cc8620ab340b355435289e523d492a2764debb
Kayo Chiaradia <kayo.chiaradia@hotmail.com>
2021-06-22 09:34:23 -0300

    add front end to project

--- src/main/java/com/chiaradia/listener/WebSocketEventListener.java
+++ src/main/java/com/chiaradia/listener/WebSocketEventListener.java
@@ -2,14 +2,14 @@ package com.chiaradia.listener;
 
 import com.chiaradia.model.ChatMessage;
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Objects;
 
 @Log4j2
 @Component
@@ -31,12 +31,10 @@ public class WebSocketEventListener {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
 
         String username = (String) headerAccessor.getSessionAttributes().get("username");
-        if(username != null) {
+        if(Objects.nonNull(username)) {
             log.info("User Disconnected : " + username);
 
-            ChatMessage chatMessage = new ChatMessage();
-            chatMessage.setType(ChatMessage.MessageType.LEAVE);
-            chatMessage.setSender(username);
+            ChatMessage chatMessage = ChatMessage.builder().type(ChatMessage.MessageType.LEAVE).sender(username).build();
 
             messagingTemplate.convertAndSend("/topic/public", chatMessage);
         }
